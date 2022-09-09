6dbec2f70474dd8fccfc338e752671adf9e2b99b
dung1993 <crorstar1704@gmail.com>
2022-09-09 16:29:33 +0700

    baitapanhcuduara

---
+++ anhcubatlam/Fibonaci.java
@@ -0,0 +1,23 @@
+package anhcubatlam;
+
+import java.util.Scanner;
+
+public class Fibonaci {
+    public static void main(String[] args) {
+        int n , number1 = 0 , number2 = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập n là số nguyên dương:");
+        n = scanner.nextInt();
+        while ( n < 0){
+            System.out.println("Nhập lại số nguyên dương bất kỳ");
+            n = scanner.nextInt();
+        }
+        for (int i = 1; i <= n; i++){
+            System.out.print(number1 + "");
+            int sum = number1 + number2;
+            number1 = number2;
+            number2 = sum;
+        }
+        System.out.print("Dãy Fibonaci với" + n + "số là:");
+    }
+}
