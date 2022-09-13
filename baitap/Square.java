a64d5afe7dbebb00f0c917892349c9f90c897d06
dung1993 <crorstar1704@gmail.com>
2022-09-13 17:41:51 +0700

    thuchanhjv

--- baitap/Square.java
+++ baitap/Square.java
@@ -12,28 +12,29 @@ public class Square {
             System.out.println("1. Draw the square");
             System.out.println("2. Draw the triangleUp");
             System.out.println("3. Draw the triangleDown");
-            System.out.println("4. Exit");
+            System.out.println("0. Exit");
             System.out.println("Enter your choice:");
             choice = scanner.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Draw the square");
+                    square();
+                    break;
+                case 2:
+                    System.out.println("Draw the triangleUp");
+                    triangleUp();
+                    break;
+                case 3:
+                    System.out.println("triangleDown");
+                    triangleDown();
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice");
+            }
         }
-        switch (choice){
-            case 1:
-                System.out.println("Draw the square");
-                square();
-                break;
-            case 2:
-                System.out.println("Draw the triangleUp");
-                triangleUp();
-                break;
-            case 3:
-                System.out.println("triangleDown");
-                triangleDown();
-                break;
-            case 0:
-                System.exit(0);
-            default:
-                System.out.println("No choice");
-        }
+
     }
 
     public static void square() {
--- vonglap/Appmenu.java
+++ vonglap/Appmenu.java
@@ -11,7 +11,7 @@ public class Appmenu {
             System.out.println("1. Draw the triangle");
             System.out.println("2. Draw the square");
             System.out.println("3. Draw the rectangle");
-            System.out.println("4. Exit");
+            System.out.println("0. Exit");
             System.out.println("Enter your choice:");
             choice = scanner.nextInt();
             switch (choice){
